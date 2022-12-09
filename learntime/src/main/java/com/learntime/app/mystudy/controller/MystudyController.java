461f56cf97b4e66db3bc6676b3eb642b6037b383
blehye <hhw0850@naver.com>
2022-12-09 14:07:24 +0900

    [추가] 내스터디 게시판템플릿 목록 화면 추가
    내스터디 게시판템플릿 목록 jsp, css, Controller 메소드 추가

--- learntime/src/main/java/com/learntime/app/mystudy/controller/MystudyController.java
+++ learntime/src/main/java/com/learntime/app/mystudy/controller/MystudyController.java
@@ -8,10 +8,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MystudyController {
 	
-	//내가 가입한 스터디게시판 메인화면
+	//내가 가입한 스터디게시판 메인 화면
 	@GetMapping("main")
 	public String main() {
 		return "mystudy/main";
+	}
+	
+	//게시판 템플릿 목록 화면
+	@GetMapping("board/list")
+	public String list() {
+		return "mystudy/board/list";
 	}
 
 }
