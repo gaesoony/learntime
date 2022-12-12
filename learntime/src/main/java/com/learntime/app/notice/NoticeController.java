13d4a1d2256528326b6c70605abbf841de93ebd7
DESKTOP-AIQ8FJP\hanac <hana.c.829@gmail.com>
2022-12-12 21:45:55 +0900

    [추가]문의게시판 리스츠조회(운영자), 상세조회

--- learntime/src/main/java/com/learntime/app/notice/NoticeController.java
+++ learntime/src/main/java/com/learntime/app/notice/NoticeController.java
@@ -12,5 +12,18 @@ public class NoticeController {
 			return "notice/noticeList";
 			
 		}
+		
+		// 문의게시판 리스트 화면(운영자)
+		@GetMapping("notice/noticeListAd")
+		public String noticeListAd() {
+			return "notice/noticeListAd";
+			
+	}
+		// 문의게시판 리스트 화면(운영자)
+		@GetMapping("notice/noticeDetail")
+		public String noticeDetail() {
+			return "notice/noticeDetail";
+			
+		}
 
 }
