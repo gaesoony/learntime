3afc35333bdcaabfb20b200b68344e4cff3a9acf
DESKTOP-FAGA4A5\Ryan <ryangwooz@gmail.com>
2022-12-19 15:32:03 +0900

    [수정] QuestionDao 어노네이션 추가

--- learntime/src/main/java/com/learntime/app/question/dao/QuestionDao.java
+++ learntime/src/main/java/com/learntime/app/question/dao/QuestionDao.java
@@ -1,9 +1,11 @@
 package com.learntime.app.question.dao;
 
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
 
 import com.learntime.app.question.vo.QuestionVo;
 
+@Repository
 public class QuestionDao {
 
 	public int questionWrite(SqlSessionTemplate sst, QuestionVo vo) {
